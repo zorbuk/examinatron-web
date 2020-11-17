f53a5b641274c31832ccd48282812bb17c77f856
miquel <miquelvalero16@gmail.com>
2020-11-17 13:29:38 +0100

    r

--- app.js
+++ app.js
@@ -3,41 +3,44 @@ const express = require('express');
 const app = express();
 
 /* { ----------- MODELS & ROUTERS ----------- } */
-require("./db/mongoose")
-const Test = require('./models/test')
-const testRouter = require('./routers/test')
+require("./db/mongoose");
+const Test = require('./models/test');
+const testRouter = require('./routers/test');
 
 /* { ----------- PORT ----------- } */
 const port = process.env.PORT || 3000
 
 /* { ----------- APP LISTEN ----------- } */
 app.listen(port, ()=>{
-    console.log(`Listening to port ${port}`)
-})
+    console.log(`Listening to port ${port}`);
+});
 
 /* { ----------- EJS VIEW ENGINE ----------- } */
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 /* { ----------- MIDDLEWARE ----------- } */
-app.use(express.static('public'))
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 app.use((req, res, next) => {
-    console.log("new request.")
+    console.log('new request made:');
+    console.log('host: ', req.hostname);
+    console.log('path: ', req.path);
+    console.log('method: ', req.method);
     next();
-})
+});
 
 /* { ----------- RUTAS WEB ----------- } */
 app.get('/', (req, res) => {
-    res.render('index', {})
-})
+    res.render('index', {});
+});
 
 /* { ----------- API ROUTING ----------- } */
 // EXAMPLE: app.use('/api', modelRouter)
-app.use('/api', testRouter)
+app.use('/api', testRouter);
 
 /* { ----------- EXPRESS ----------- } */
 app.use((req, res) => {
-    res.status(404).render('404', { })
-})
+    res.status(404).render('404', {});
+});
